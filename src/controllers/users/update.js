93fa56a213920ad796248969b975171849eb91c4
Jonatas Sennas <87510640+apocsenpai@users.noreply.github.com>
2023-01-29 15:30:18 -0300

    Feat/update user (#7)
    * feat(cart): create cart update route
    
    * feat(user): create index user route

---
+++ src/controllers/users/update.js
@@ -0,0 +1,39 @@
+import chalk from "chalk";
+import { users } from "../../config/database.js";
+import internalError from "../../utils/functions/internalError.js";
+import sanitizeObject from "../../utils/functions/sanitizeObject.js";
+import bcrypt from "bcrypt";
+import { saltRounds } from "../../utils/constants/bcrypt.js";
+import { ObjectId } from "mongodb";
+
+const updateUser = async (req, res) => {
+  const { userId } = res.locals;
+  const { name, email, password, address, cart } = res.sanitizedParams;
+  let hashedPassword = "";
+  console.log(chalk.cyan("PUT /users"));
+  try {
+    const user = await users.findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).send({ message: "Usuário não encontrado!" });
+    }
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, saltRounds);
+    }
+    await users.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          name: name || user.name,
+          email: email || user.email,
+          password: hashedPassword || user.password,
+          address: address || user.address,
+          cart: cart || user.cart,
+        },
+      }
+    );
+    res.sendStatus(200);
+  } catch (error) {
+    internalError(error, res);
+  }
+};
+export default updateUser;
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import createUser from "../controllers/users/create.js";
+import indexUserById from "../controllers/users/index.js";
+import updateUser from "../controllers/users/update.js";
+import authenticate from "../middlewares/authenticate.js";
 import validateSchema from "../middlewares/validateSchema.js";
 import createUserSchema from "../request_schemas/users/create.js";
+import updateUserSchema from "../request_schemas/users/update.js";
 
 const router = Router("/users");
 
 router.post("/", validateSchema(createUserSchema), createUser);
 
+router.use(authenticate);
+router.put("/", validateSchema(updateUserSchema), updateUser);
+router.get("/", indexUserById);
 export { router as usersRouter };
