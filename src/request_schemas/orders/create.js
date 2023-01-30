5211c0dfa0311b6458e2747bd6d6857e2770fe53
Kevin Alves <kevin.alves.dev@gmail.com>
2023-01-30 09:54:44 -0300

    add block for empty cart checkout

--- src/request_schemas/orders/create.js
+++ src/request_schemas/orders/create.js
@@ -4,7 +4,7 @@ import cartSchema from "../cart.js";
 const createOrderSchema = Joi.object({
   address: Joi.string().required(),
   paymentMethod: Joi.string().valid("pix", "credit card", "bank slip", "xerecard").required(),
-  cart: Joi.array().items(cartSchema).required()
+  cart: Joi.array().items(cartSchema).min(1).required()
 });
 
 export default createOrderSchema;
